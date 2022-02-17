0ba429d7fbe23fb5274c2109a504af9300aaf200
Rafael Waldo Delgado Doblas <>
2022-02-17 13:29:45 +0100

    Mechatron UART

--- BusBlasterV3_Playstation_2_UART.v
+++ BusBlasterV3_Playstation_2_UART.v
@@ -5,9 +5,9 @@
 // Project Name:   BusBlasterV3_Playstation_2_UART
 // Target Devices: XC2C32A_VQ44
 // Tool versions:  ISE 14.7
-// Description:    Buffer to connect to the Playstation 2 UART. The UART level is
+// Description:    Buffer to connect to the Playstation 2 UARTs. The UART level is
 //                 1.6 volts. This buffer only supports to receive data from the PS2.
-//                 RX must be connected to nTRST, the vTarget jumper must be removed
+//                 TX must be connected to nTRST, the vTarget jumper must be removed
 //                 and vTarget should be connected to the Playstation 2 1.6V power rail.
 //
 // Revision: 
@@ -19,12 +19,19 @@ module BusBlasterV3_Playstation_2_UART(
 
 	// FT AD BUS	
 	output  FT_AD1_TDI,
+	input   FT_AD0_TCK,
 	
 	// JTAG Connector
-	input nTRST
-
+	input   nTRST, JP2_P28, BUTTON, TCK,
+	output  TDI, JP2_P19_LED, RTCK, TMS
+	
 );
 
-	assign FT_AD1_TDI = nTRST;
+	assign FT_AD1_TDI = JP2_P28? nTRST : TCK; // FT_RDX1       <- JP2_P28? EE_TDX  : MECHATRON_TDX
+	assign TDI = JP2_P28? FT_AD0_TCK : 'bZ;   // EE_RDX        <- JP2_P28? FT_TDX1 : Z
+	assign RTCK = JP2_P28? 'bZ : FT_AD0_TCK;  // MECHATRON_RDX <- JP2_P28? Z       : FT_TDX1
+
+	assign JP2_P19_LED = JP2_P28;
+	assign TMS = BUTTON? 'bZ : 0; // MECHATRON_TESTMODE
 
 endmodule
